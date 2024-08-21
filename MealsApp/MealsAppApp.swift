12dedccb38bbc6c05495c57c0522d6f59249273c
AminPlusPlus <amin030206@gmail.com>
2024-08-20 21:13:26 -0500

    Initial Commit

---
+++ MealsApp/MealsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsAppApp.swift
+//  MealsApp
+//
+//  Created by Aminjoni Abdullozoda on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MealsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
